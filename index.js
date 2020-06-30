9ac1e4126dbaaff57c7309c584f58e23b0dfe1bd
mm1025web <mm9084@srmist.edu.in>
2020-06-30 22:13:59 +0530

    added files

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+ 
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car img');
+    let flag = true;
+
+    const cars=['./images/Img_05.png','./images/Img_06.png']
+
+    $(document).on('keypress',function(e){
+       // console.log(e.which); to get keycode
+        if(e.which == 13){
+            $($surface).toggleClass('moveRight');
+            $($car).toggleClass('suspension');
+        }
+    })
+    $(document).on('keypress',function(e){
+         if(e.which==32){
+             if(flag){
+                 flag = false;
+                 $img.attr('src',cars[0]);
+             }
+             else{
+                 flag = true;
+                 $img.attr('src',cars[1]);
+
+             }
+         }
+     })
+
+});
