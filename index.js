82654030a26905c1808ed6995d74cf90162e87ae
mm1025web <mm9084@srmist.edu.in>
2020-07-01 01:35:39 +0530

    added audio

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
-$(document).ready(function(){
+if($(window).width() >= 800){
+  $(document).ready(function(){
  
-    alert("Press Enter to start and Spacebar to turn off the lights.")
+        alert("Press Enter to start and Spacebar to turn off the lights.")
+        $surface = $('.surface');
+        $car = $('.car');
+        $img = $('.car img');
+        let flag = true;
+
+        const cars=['./images/car2.png','./images/car1.png']
+
+        $(document).on('keypress',function(e){
+        // console.log(e.which); to get keycode
+            if(e.which == 13){
+                $('audio#start')[0].play();
+                $($surface).toggleClass('moveRight');
+                $($car).toggleClass('suspension');
+            }
+            if(e.which == 119){
+                $($surface).toggleClass('moveFast');
+                $('audio#fast')[0].play();
+
+            }
+        })
+        $(document).on('keypress',function(e){
+            if(e.which==32){
+                if(flag){
+                    flag = false;
+                    $img.attr('src',cars[0]);
+                }
+                else{
+                    flag = true;
+                    $img.attr('src',cars[1]);
+
+                }
+            }
+        })
+
+    });
+
+    $(window).ready(function(){
+        setTimeout(function(){
+        alert("Press W to speed up!")
+        }, 4000);
+    });
+}
+else{
     $surface = $('.surface');
     $car = $('.car');
-    $img = $('.car img');
-    let flag = true;
-
-    const cars=['./images/car2.png','./images/car1.png']
-
-    $(document).on('keypress',function(e){
-       // console.log(e.which); to get keycode
-        if(e.which == 13){
-            $($surface).toggleClass('moveRight');
+    $(document).ready(function(){
+            $($surface).toggleClass('moveMed');
             $($car).toggleClass('suspension');
-        }
-        if(e.which == 119){
-            $($surface).toggleClass('moveFast');
-        }
-    })
-    $(document).on('keypress',function(e){
-         if(e.which==32){
-             if(flag){
-                 flag = false;
-                 $img.attr('src',cars[0]);
-             }
-             else{
-                 flag = true;
-                 $img.attr('src',cars[1]);
-
-             }
-         }
-     })
-
-});
-$(window).ready(function(){
-    setTimeout(function(){
-       alert("Press W to speed up!")
-    }, 4000);
- });
+    });
+}
