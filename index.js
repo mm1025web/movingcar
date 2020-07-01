320753dbb29a05dd12a62adb21d1664be636716f
mm1025web <mm9084@srmist.edu.in>
2020-07-01 14:08:43 +0530

    added sound effects

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ if($(window).width() >= 700){
   $(document).ready(function(){
  
         // alert("Press Enter to start and Spacebar to turn off the lights.")
+        $night = $('.night')
         $surface = $('.surface');
         $car = $('.car');
         $img = $('.car img');
+
         let flag = true;
 
         const cars=['./images/car2.png','./images/car1.png'];
@@ -13,11 +15,14 @@ if($(window).width() >= 700){
         // console.log(e.which); to get keycode
             if(e.which == 13){
                 $('audio#start')[0].play();
+               $($night).toggleClass('slide');
                $($surface).toggleClass('moveRight');
                 $($car).toggleClass('suspension');
             }
             if(e.which == 119){
                 $($surface).toggleClass('moveFast');
+               $($night).toggleClass('slideFast');
+
                 $('audio#fast')[0].play();
 
             }
@@ -31,9 +36,16 @@ if($(window).width() >= 700){
                 else{
                     flag = true;
                     $img.attr('src',cars[1]);
-
                 }
             }
+            if(e.which == 104){
+                   $('audio#horn')[0].play();
+            }
+            if(e.which == 97){
+                $('audio#acc')[0].play();
+            }
+         
+
         })
 
     });
@@ -53,9 +65,76 @@ if($(window).width() >= 700){
             $('#myModal3').modal('show');
         }, 12000);
      });
+ 
+ 
+     var lFollowX = 0,
+            lFollowY = 0,
+            x = 0,
+            y = 0,
+            friction = 1 / 30;
+
+        function moveBackground() {
+        x += (lFollowX - x) * friction;
+        y += (lFollowY - y) * friction;
+        
+        translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+        $('.night').css({
+            '-webit-transform': translate,
+            '-moz-transform': translate,
+            'transform': translate
+        });
+
+        window.requestAnimationFrame(moveBackground);
+        }
+
+        $(window).on('mousemove click', function(e) {
+
+        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+        lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+        lFollowY = (10 * lMouseY) / 100;
+
+        });
+
+        moveBackground();
 }
-if($(window).width() <= 700){
-    
+
+
+else if($(window).width() <= 700){
+            var lFollowX = 0,
+            lFollowY = 0,
+            x = 0,
+            y = 0,
+            friction = 1 / 30;
+
+        function moveBackground() {
+        x += (lFollowX - x) * friction;
+        y += (lFollowY - y) * friction;
+
+        translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+        $('.night').css({
+            '-webit-transform': translate,
+            '-moz-transform': translate,
+            'transform': translate
+        });
+
+        window.requestAnimationFrame(moveBackground);
+        }
+
+        $(window).on('mousemove click', function(e) {
+
+        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+        lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+        lFollowY = (10 * lMouseY) / 100;
+
+        });
+
+        moveBackground();
+
+
     $surface = $('.surface');
     $car = $('.car');
     $img = $('.car img');
